1bd4ea770a9d19ae695c7236f653e2ba3bc02903
Meedoriya <meedoriya@gmail.com>
2024-02-21 15:52:41 +0600

    creating project

---
+++ src/main/java/com/alibi/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.alibi.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibi/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibi.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
