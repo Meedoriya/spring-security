8cc4e35f121982d3227e4661e36cd35b7ba782b7
Meedoriya <meedoriya@gmail.com>
2024-02-24 23:01:29 +0500

    adding authorities instead of roles

--- src/main/java/com/alibi/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/alibi/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.alibi.springsecurity.config;
 
+import com.alibi.springsecurity.model.Permission;
 import com.alibi.springsecurity.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,9 +29,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequest ->
                         authorizeRequest
                                 .requestMatchers("/").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                                .requestMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
-                                .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                                .requestMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+                                .requestMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+                                .requestMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
@@ -43,12 +44,12 @@ public class SecurityConfig {
                 User.builder()
                         .username("admin")
                         .password(passwordEncoder().encode("admin"))
-                        .roles(Role.ADMIN.name())
+                        .authorities(Role.ADMIN.getAuthorities())
                         .build(),
                 User.builder()
                         .username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
